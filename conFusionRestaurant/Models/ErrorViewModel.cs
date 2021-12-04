ed5b581a1932202cb2091da8fa48b20499cb320d
Nabil Helmy <43821886+nabilhelmy313@users.noreply.github.com>
2021-12-04 19:27:12 +0200

    Add project files.

---
+++ conFusionRestaurant/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace conFusionRestaurant.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
